177e131361ac511b5b82033507f79db0c4ee43ec
arulk8 <arulmurugan194@gmail.com>
2016-11-28 13:05:46 +0530

    google undefined

--- script/app.js
+++ script/app.js
@@ -1,14 +1,41 @@
-
 var map;
-var markers = [];
-
 var locations = [
-          {title: 'Madras High Court', location: {lat: 13.087191, lng: 80.286434 }},
-          {title: 'Indian Institute Of Logistics', location: {lat: 13.085971, lng: 80.284127 }},
-          {title: 'Reserve Bank of India', location: {lat: 13.084453, lng: 80.288579 }},
-          {title: 'TNPSC', location: {lat: 13.085247, lng: 80.282035 }},
-	      {title: 'TAMIL NADU GOVERNMENT DENTAL COLLEGE', location: {lat: 13.084975, lng: 80.282604}},         
-	      {title: 'Southern Railway Divisional Office', location: {lat:13.08254 , lng:80.277357 }}
+          {
+            title: 'Madras High Court', 
+            location: {
+              lat: 13.087191,
+               lng: 80.286434 }
+             },
+          {
+            title: 'Indian Institute Of Logistics', 
+            location: {
+              lat: 13.085971,
+               lng: 80.284127 }
+             },
+          {
+            title: 'Reserve Bank of India',
+             location: {
+              lat: 13.084453,
+               lng: 80.288579 }
+             },
+          {
+            title: 'TNPSC', 
+            location: {
+              lat: 13.085247, 
+              lng: 80.282035 }
+            },
+	      {
+          title: 'TAMIL NADU GOVERNMENT DENTAL COLLEGE',
+           location: {
+            lat: 13.084975,
+             lng: 80.282604}
+           },         
+	      {
+          title: 'Southern Railway Divisional Office',
+           location: {
+            lat:13.08254 , 
+            lng:80.277357 }
+          }
 ];
 function initMap() {
 
@@ -18,46 +45,33 @@ function initMap() {
    zoom: 15
 
   });
-    var largeInfowindow = new google.maps.InfoWindow();
-     var bounds = new google.maps.LatLngBounds();
+ }
 
-for (var i=0;i<locations.length; i++)
-{
-	var position= locations[i].location;
-	var title =locations[i].title;
+  
 
-	var marker =new google.maps.Marker(
-		{
-		map: map,
-		position: position,
-		title: title,
-		animation: google.maps.Animation.Drop,
-		id:i 
-	});
-	markers.push(marker); 
+var viewModel =function()    
+{ "use strict";
+ var self = this;
 
-	marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
-        });
+ self.markers=[];
 
-	
-	bounds.extend(markers[i].position);
-}
+ self.Locations =ko.observableArray(locations);
 
-map.fitBounds(bounds);
+ self.Locations().forEach(function(loc){
 
-}
+  var marker = new google.maps.Marker({
+    position :loc.location,
+    title:loc.title,
+    map :map,
+    animation: google.maps.Animation.Drop
+  });
+   loc.marker =marker;
+   self.markers.push(marker);
+});
 
 
- function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker(null);
-          });
-        }
-      }
+    };
+
+    ko.applyBindings(new viewModel());
+
+  
